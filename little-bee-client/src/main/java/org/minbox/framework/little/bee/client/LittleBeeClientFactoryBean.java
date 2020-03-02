428a5893dc357f779a41c152834fd07202bed1e1
hengboy <jnyuqy@gmail.com>
2020-03-02 16:55:06 +0800

    初始化项目结构

---
+++ little-bee-client/src/main/java/org/minbox/framework/little/bee/client/LittleBeeClientFactoryBean.java
@@ -0,0 +1,11 @@
+package org.minbox.framework.little.bee.client;
+
+/**
+ * The factory bean for build "LittleBee" client
+ * <p>
+ * Define all the configurations required by the client
+ *
+ * @author 恒宇少年
+ */
+public class LittleBeeClientFactoryBean {
+}
