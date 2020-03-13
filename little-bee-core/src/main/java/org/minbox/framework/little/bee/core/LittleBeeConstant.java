7dc8689d3971dec08d619bdaec12236afb2abc8b
hengboy <jnyuqy@gmail.com>
2020-03-13 10:41:04 +0800

    添加常量定义接口

---
+++ little-bee-core/src/main/java/org/minbox/framework/little/bee/core/LittleBeeConstant.java
@@ -0,0 +1,35 @@
+package org.minbox.framework.little.bee.core;
+
+/**
+ * Public constant definition
+ * <p>
+ * provide all constants that can be used by the system architecture
+ *
+ * @author 恒宇少年
+ */
+public interface LittleBeeConstant {
+    /**
+     * Empty string
+     */
+    String EMPTY_STRING = "";
+    /**
+     * Space
+     */
+    String SPACE = " ";
+    /**
+     * The "ssh" bash
+     */
+    String SSH_BASH = "ssh";
+    /**
+     * The "cd" ssh bash
+     */
+    String SSH_CD = "cd";
+    /**
+     * Quotation marks
+     */
+    String QUOTE = "\"";
+    /**
+     * Semicolon
+     */
+    String SEMICOLON = ";";
+}
