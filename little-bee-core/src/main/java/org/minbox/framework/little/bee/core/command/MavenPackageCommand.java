1cf1fdffbf6034efd8515749fc54a5b3814d1d2a
hengboy <jnyuqy@gmail.com>
2020-03-29 20:42:05 +0800

    修改使用preExecute方法进行设置命令行参数项

--- little-bee-core/src/main/java/org/minbox/framework/little/bee/core/command/MavenPackageCommand.java
+++ little-bee-core/src/main/java/org/minbox/framework/little/bee/core/command/MavenPackageCommand.java
@@ -10,7 +10,8 @@ package org.minbox.framework.little.bee.core.command;
  * @author 恒宇少年
  */
 public class MavenPackageCommand extends MavenCommand {
-    public MavenPackageCommand() {
+    @Override
+    public void preExecute() {
         setCommandOptions(new String[]{MAVEN_CLEAN, MAVEN_PACKAGE});
     }
 }
