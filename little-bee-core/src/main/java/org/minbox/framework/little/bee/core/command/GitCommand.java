daba1a750a38ea8b5c86eb9bd29e9fc6c7d1059c
hengboy <jnyuqy@gmail.com>
2020-03-30 09:41:17 +0800

    添加获取项目目录、设置项目目录的公共方法

--- little-bee-core/src/main/java/org/minbox/framework/little/bee/core/command/GitCommand.java
+++ little-bee-core/src/main/java/org/minbox/framework/little/bee/core/command/GitCommand.java
@@ -1,5 +1,9 @@
 package org.minbox.framework.little.bee.core.command;
 
+import org.minbox.framework.little.bee.core.LittleBeeConstant;
+import org.minbox.framework.little.bee.core.tools.FileTools;
+import org.springframework.util.ObjectUtils;
+
 /**
  * @author 恒宇少年
  */
@@ -20,8 +24,42 @@ public abstract class GitCommand extends AbstractCommand {
      * Pull specified branch parameters
      */
     static final String GIT_BRANCH = "-b %s";
+    /**
+     * Subdirectory for log files
+     */
+    private static final String PROJECT_SUB_PATH = "/projects/";
 
     public GitCommand() {
         setBash(GIT_BASH);
+    }
+
+
+    /**
+     * Get the storage location of the project
+     * <p>
+     * if the location is executed through the {@link #setExecutionDirectory(String)} method,
+     * use it directly, otherwise use the default location
+     *
+     * @return The project storage location
+     */
+    protected String getProjectDirectory() {
+        String directory = getExecutionDirectory();
+        if (ObjectUtils.isEmpty(directory)) {
+            String defaultExecutionDirectory = String.format("%s/%s%s",
+                System.getProperty(LittleBeeConstant.USER_HOME),
+                LittleBeeConstant.LITTLE_BEE_DIR,
+                PROJECT_SUB_PATH);
+            return defaultExecutionDirectory;
+        }
+        return directory;
+    }
+
+    /**
+     * Create the storage location of the project
+     *
+     * @param projectDirectory The project storage location directory
+     */
+    protected void createProjectDirectory(String projectDirectory) {
+        FileTools.createDirectory(projectDirectory);
     }
 }
