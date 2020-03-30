89a7445496e4bd462d630b1cb4d02cb80847c8ec
hengboy <jnyuqy@gmail.com>
2020-03-30 09:40:49 +0800

    git clone命令执行前检查默认存放项目目录是否存在

--- little-bee-core/src/main/java/org/minbox/framework/little/bee/core/command/GitCloneCommand.java
+++ little-bee-core/src/main/java/org/minbox/framework/little/bee/core/command/GitCloneCommand.java
@@ -1,7 +1,5 @@
 package org.minbox.framework.little.bee.core.command;
 
-import org.minbox.framework.little.bee.core.LittleBeeConstant;
-import org.minbox.framework.little.bee.core.tools.FileTools;
 import org.springframework.util.Assert;
 import org.springframework.util.ObjectUtils;
 
@@ -19,10 +17,6 @@ public class GitCloneCommand extends GitCommand {
      * clone the branch of the project, the default is master
      */
     private String branch = "master";
-    /**
-     * Subdirectory for log files
-     */
-    private static final String PROJECT_SUB_PATH = "/projects/";
 
     /**
      * Set the project git address
@@ -39,7 +33,9 @@ public class GitCloneCommand extends GitCommand {
      * @param branch project branch name
      */
     public void setBranch(String branch) {
-        this.branch = branch;
+        if (!ObjectUtils.isEmpty(branch)) {
+            this.branch = branch;
+        }
     }
 
     /**
@@ -51,38 +47,11 @@ public class GitCloneCommand extends GitCommand {
     void preExecute() {
         Assert.notNull(this.projectAddress, "The address of the clone project cannot be null.");
         String cloneBranch = String.format(GIT_BRANCH, branch);
-        String projectStorageDirectory = getProjectStorageDirectory();
-        createProjectStorageDirectory(projectStorageDirectory);
-        setCommandOptions(new String[]{GIT_CLONE, cloneBranch, projectAddress});
-    }
-
-    /**
-     * Get the storage location of the project
-     * <p>
-     * if the location is executed through the {@link #setExecutionDirectory(String)} method,
-     * use it directly, otherwise use the default location
-     *
-     * @return The project storage location
-     */
-    private String getProjectStorageDirectory() {
-        String directory = getExecutionDirectory();
-        if (ObjectUtils.isEmpty(directory)) {
-            String defaultExecutionDirectory = String.format("%s/%s%s",
-                System.getProperty(LittleBeeConstant.USER_HOME),
-                LittleBeeConstant.LITTLE_BEE_DIR,
-                PROJECT_SUB_PATH);
-            setExecutionDirectory(defaultExecutionDirectory);
-            return defaultExecutionDirectory;
+        String projectStorageDirectory = getProjectDirectory();
+        if (!projectStorageDirectory.equals(getExecutionDirectory())) {
+            setExecutionDirectory(projectStorageDirectory);
         }
-        return directory;
-    }
-
-    /**
-     * Create the storage location of the project
-     *
-     * @param projectDirectory The project storage location directory
-     */
-    private void createProjectStorageDirectory(String projectDirectory) {
-        FileTools.createDirectory(projectDirectory);
+        createProjectDirectory(projectStorageDirectory);
+        setCommandOptions(new String[]{GIT_CLONE, cloneBranch, projectAddress});
     }
 }
