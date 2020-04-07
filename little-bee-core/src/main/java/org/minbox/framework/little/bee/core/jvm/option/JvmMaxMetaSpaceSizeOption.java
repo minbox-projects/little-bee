cd42e745693aedbd246d3a1241c3cb9e97a432dc
hengboy <jnyuqy@gmail.com>
2020-04-07 21:21:13 +0800

    "-XX:MaxMetaspaceSize" Jvm参数项实现

---
+++ little-bee-core/src/main/java/org/minbox/framework/little/bee/core/jvm/option/JvmMaxMetaSpaceSizeOption.java
@@ -0,0 +1,36 @@
+package org.minbox.framework.little.bee.core.jvm.option;
+
+import org.minbox.framework.little.bee.core.LittleBeeConstant;
+import org.minbox.framework.little.bee.core.jvm.AbstractJvmOption;
+import org.minbox.framework.little.bee.core.jvm.JvmOptionType;
+
+/**
+ * The "-XX:MaxMetaspaceSize" jvm option
+ *
+ * <pre class="code">
+ *      String maxMetaSpaceSize = JvmOptionFactory.getJvmOption(JvmMaxMetaSpaceSizeOption.class,
+ *          JvmOptionSize.MB.value(128).toString()).format();
+ *      // -XX:MaxMetaspaceSize=128m
+ *      System.out.println(maxMetaSpaceSize);
+ * </pre>
+ *
+ * @author 恒宇少年
+ */
+public class JvmMaxMetaSpaceSizeOption extends AbstractJvmOption {
+    private static final String JVM_MAX_META_SPACE_SIZE = "MaxMetaspaceSize";
+
+    @Override
+    public String getOptionName() {
+        return JVM_MAX_META_SPACE_SIZE;
+    }
+
+    @Override
+    public String getAssignmentSymbol() {
+        return LittleBeeConstant.ASSIGNMENT_EQ;
+    }
+
+    @Override
+    public JvmOptionType getOptionType() {
+        return JvmOptionType.NonStable;
+    }
+}
