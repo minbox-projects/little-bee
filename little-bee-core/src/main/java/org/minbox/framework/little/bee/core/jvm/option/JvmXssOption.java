b3702f577cc612d7a4d282b8c6e2bd39c91ff573
hengboy <jnyuqy@gmail.com>
2020-04-07 21:22:23 +0800

    "-Xss" Jvm参数项实现

---
+++ little-bee-core/src/main/java/org/minbox/framework/little/bee/core/jvm/option/JvmXssOption.java
@@ -0,0 +1,29 @@
+package org.minbox.framework.little.bee.core.jvm.option;
+
+import org.minbox.framework.little.bee.core.jvm.AbstractJvmOption;
+import org.minbox.framework.little.bee.core.jvm.JvmOptionType;
+
+/**
+ * The "-Xss" jvm option
+ * <pre class="code">
+ *      String xss = JvmOptionFactory.getJvmOption(JvmXssOption.class,
+ *             JvmOptionSize.KB.value(256).toString()).format();
+ *      // -Xss256k
+ *      System.out.println(xss);
+ * </pre>
+ *
+ * @author 恒宇少年
+ */
+public class JvmXssOption extends AbstractJvmOption {
+    private static final String JVM_XSS = "ss";
+
+    @Override
+    public String getOptionName() {
+        return JVM_XSS;
+    }
+
+    @Override
+    public JvmOptionType getOptionType() {
+        return JvmOptionType.NonStandard;
+    }
+}
