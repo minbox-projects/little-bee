592c275da33ed23c5fdb02b815f7f9654d96d50c
hengboy <jnyuqy@gmail.com>
2020-04-07 21:20:27 +0800

    "-server" Jvm参数项实现

---
+++ little-bee-core/src/main/java/org/minbox/framework/little/bee/core/jvm/option/JvmServerOption.java
@@ -0,0 +1,28 @@
+package org.minbox.framework.little.bee.core.jvm.option;
+
+import org.minbox.framework.little.bee.core.jvm.AbstractJvmOption;
+import org.minbox.framework.little.bee.core.jvm.JvmOptionType;
+
+/**
+ * The "-server" jvm option
+ * <pre class="code>
+ *     String server = JvmOptionFactory.getJvmOption(JvmServerOption.class, LittleBeeConstant.EMPTY_STRING).format();
+ *     // -server
+ *     System.out.println(server);
+ * </pre>
+ *
+ * @author 恒宇少年
+ */
+public class JvmServerOption extends AbstractJvmOption {
+    private static final String JVM_SERVER = "server";
+
+    @Override
+    public String getOptionName() {
+        return JVM_SERVER;
+    }
+
+    @Override
+    public JvmOptionType getOptionType() {
+        return JvmOptionType.Standard;
+    }
+}
