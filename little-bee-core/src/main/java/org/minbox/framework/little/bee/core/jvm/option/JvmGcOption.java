1591502d6e6a83b80a8cccbb26d321866457511d
hengboy <jnyuqy@gmail.com>
2020-04-07 21:19:56 +0800

    垃圾回收器Jvm参数项实现

---
+++ little-bee-core/src/main/java/org/minbox/framework/little/bee/core/jvm/option/JvmGcOption.java
@@ -0,0 +1,35 @@
+package org.minbox.framework.little.bee.core.jvm.option;
+
+import org.minbox.framework.little.bee.core.LittleBeeConstant;
+import org.minbox.framework.little.bee.core.jvm.AbstractJvmOption;
+import org.minbox.framework.little.bee.core.jvm.JvmOptionType;
+
+/**
+ * The {@link org.minbox.framework.little.bee.core.jvm.JvmGc} jvm option
+ * <pre class="code">
+ *   String gc = JvmOptionFactory.getJvmOption(JvmGcOption.class, JvmGc.UseConcMarkSweepGC.getValue()).format();
+ *   // -XX:+UseConcMarkSweepGC
+ *   System.out.println(gc);
+ * </pre>
+ *
+ * @author 恒宇少年
+ * @see org.minbox.framework.little.bee.core.jvm.JvmGc
+ */
+public class JvmGcOption extends AbstractJvmOption {
+    /**
+     * Get jvm option name
+     * <p>
+     * gc option name does not return anything
+     *
+     * @return Jvm option name
+     */
+    @Override
+    public String getOptionName() {
+        return LittleBeeConstant.EMPTY_STRING;
+    }
+
+    @Override
+    public JvmOptionType getOptionType() {
+        return JvmOptionType.NonStable;
+    }
+}
